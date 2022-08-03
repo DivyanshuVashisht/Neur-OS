96f92ddf9fce90b9d350761b5a4833ac1abd2eb7
Divyanshu Vashisht <divyanshuvashisht2001@gmail.com>
2022-08-03 23:23:53 +0530

    Created Virtual Keyboard Layer

--- src/kernel.c
+++ src/kernel.c
@@ -17,6 +17,7 @@
 #include "gdt/gdt.h"
 #include "config.h"
 #include "memory/memory.h"
+#include "keyboard/keyboard.h"
 #include "task/tss.h"
 
 uint16_t* video_mem = 0;
@@ -125,6 +126,9 @@ void kernel_main(){
 
     // Register the kernel commands
     isr80h_register_commands();
+
+    // Initialize all the system keyboards
+    keyboard_init();
 
     struct process* process = 0;
     int res = process_load("0:/blank.bin", &process);
