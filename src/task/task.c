0214b6f565923d398fa454d33c8b415f9813999a
Divyanshu Vashisht <divyanshuvashisht2001@gmail.com>
2022-08-12 16:25:58 +0530

    Implementing an elf-loader that can run static elf files

--- src/task/task.c
+++ src/task/task.c
@@ -6,6 +6,7 @@
 #include "string/string.h"
 #include "process.h"
 #include "idt/idt.h"
+#include "loader/formats/elfloader.h"
 
 // The current task that is running
 struct task* current_task = 0;
@@ -183,6 +184,9 @@ int task_init(struct task* task, struct process* process){
     }
 
     task->registers.ip = NEUROS_PROGRAM_VIRTUAL_ADDRESS;
+    if (process->filetype == PROCESS_FILETYPE_ELF){
+        task->registers.ip = elf_header(process->elf_file)->e_entry;
+    }
     task->registers.ss = USER_DATA_SEGMENT;
     task->registers.cs = USER_CODE_SEGMENT;
     task->registers.esp = NEUROS_PROGRAM_VIRTUAL_STACK_ADDRESS_START;
