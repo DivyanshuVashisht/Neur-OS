49bb809659bb85bae8e306ac79e25546303af88d
Divyanshu Vashisht <divyanshuvashisht2001@gmail.com>
2022-08-16 01:06:47 +0530

    Creating some important stdlib functions

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -1,8 +1,16 @@
 #include "neuros.h"
 #include "stdlib.h"
 #include "stdio.h"
+#include "string.h"
 
 int main(int argc, char** argv){
-    printf("My age is %i\n", 100);
+    char words[] = "hello how are you?";
+
+    const char* token = strtok(words, " ");
+
+    while(token){
+        printf("%s\n", token);
+        token = strtok(NULL, " ");
+    }
     return 0;
 }
