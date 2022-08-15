b722bb29ea4b10d7330706bda9946739fd15fdc7
Divyanshu Vashisht <divyanshuvashisht2001@gmail.com>
2022-08-15 22:06:05 +0530

    Implementing a shell

---
+++ programs/shell/src/shell.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "neuros.h"
+
+int main(int argc, char** argv){
+    print("Neur-OS v1.0.0\n");
+    while(1) {
+        print("> ");
+        char buf[1024];
+        neuros_terminal_readline(buf, sizeof(buf), true);
+        print("\n");
+        print(buf);
+        print("\n");
+    }
+    return 0;
+}
